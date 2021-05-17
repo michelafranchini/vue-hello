128553795983707ff31c37a4e1b93f2707d0072c
michelafranchini <michela.franchini.91@gmail.com>
2021-05-17 15:16:48 +0200

    Cambio con button

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ var app = new Vue(
         data: {
             messaggio: "Eccoci finalmente su VueJS!", 
             immagine: "https://www.luigisabbetti.it/wp-content/uploads/2018/04/guida-vue-js-italiano.png"
+        }, 
+        methods: {
+            cambiaImmagine:function () {
+                this.immagine = "https://image.slidesharecdn.com/davidlicenvuejs-slides-170526074856/95/vuejs-introduction-1-638.jpg?cb=1495785021"; 
+            }
         }
     }
 ); 
